29a883f388ee1c98752c64131f7248ac47754dc9
Solomon <kyobenimaru001@gmail.com>
2024-04-27 19:33:02 +0300

    commit for the intro of the BaseModel and User classes to handle object creation to be saved in the db

--- PropertyPulse_Django/Users/views.py
+++ PropertyPulse_Django/Users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-
+# from django.contrib.auth.forms import UserCreationForm
+from .models import *
 
 def login(requests):
     return render(requests, 'Users/login.html')
@@ -9,12 +9,17 @@ def login(requests):
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Account created successfully!')
-            return redirect('login')
+        firstName = request.form['first_name']
+        secondName = request.form['second_name']
+        email = request.form['email']
+        password = request.form['password']
+        
+        user = User(firstName,secondName,email,password)
+        user.save()
+        
+        messages.success(request, 'Account created successfully!')
+        return redirect('login')
     else:
-        form = UserCreationForm()
-    return render(request, 'Users/register.html', {'form': form})
+        # form = UserCreationForm()
+        return render(request, 'Users/register.html')
 # removed context var on the render line, {'form': form}
