4a81a44049ea72c8a04b74ab5847dd1caf14ed4f
mitsudata <solomonkaiaru154@gmail.com>
2024-11-01 16:09:47 +0300

    Update: Optimise usr Exerience

--- PropertyPulse_flask/migration.py
+++ PropertyPulse_flask/migration.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
 
-from db_storage import DBStorage
+# from .models import DBStorage
 from sqlalchemy import create_engine, MetaData
-from db_storage import Base
+from models.db_storage import Base
 import os
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from user import User, Base
-from property import Property
-from reviews import Review
+from models.user import User
+from models.property import Property
+from models.reviews import Review
 
 # Create an engine that connects to the MySQL database
-engine = create_engine('mysql+mysqldb://root:10229101151@192.168.1.6/flask_property_pulse')
+engine = create_engine('mysql+mysqldb://mitsudata:10229101151@localhost/flask_property_pulse')
 
 # Create all tables in the database
 Base.metadata.create_all(engine)
